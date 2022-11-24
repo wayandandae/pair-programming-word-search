890b736224bc902ee454a18f270703b406698523
Jack Kang <jkang0724@hotmail.com>
2022-11-24 13:16:28 -0500

    Week02/Day03 - Word Search Pair Programming Challenge with @willsmores

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,12 +15,12 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +31,56 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+  
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'N', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'E', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'T', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'S', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FASTEN');
 
     assert.isTrue(result);
+  });
+
+  it("should return true if the word is present diagonally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'M', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'A', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'Z', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'I', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'N', 'R', 'T', 'N', 'E', 'Y', 'B'],
+      ['G', 'B', 'T', 'E', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'N', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'AMAZING');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if no string is entered", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+
+    assert.isFalse(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,131 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// transpose a 2D array diagonally, then return an array with its width and length switched
+const transpose = matrix => {
+  // create a variable to hold the resulting array
+  const result = [];
+  // error message for different array length than the first array
+  const lError = "Every array's length should match the length of the first array. Please try again.";
+  // number of elements of the first array as matrix width
+  const matrixW = matrix[0].length;
+  // number of arrays as matrix height
+  const matrixH = matrix.length;
 
-module.exports = wordSearch
+  // iterate through each array
+  matrix.forEach(array => {
+    // if the length of another array is different than matrix width, throw the error message
+    if (array.length !== matrixW) throw new Error(lError);
+  });
+
+  // iterate through each array, until width of array is reached
+  for (let i = 0; i < matrixW; i++) {
+    // create a varaible to hold each line of array
+    result[i] = [];
+    // iterate through each element of array, until height of array
+    for (let j = 0; j < matrixH; j++) {
+      // push element of matching index from each array
+      result[i].push(matrix[j][i]);
+    }
+  }
+
+  // return the resulting transposed array
+  return result;
+};
+
+// add backward string values to array
+const addBWard = array => {
+  // for every string item of array
+  array.forEach(string => {
+    // if string length is longer than 1, push the item in reverse order
+    if (string.length > 1) array.push(string.split('').reverse().join(''));
+  });
+
+  return array;
+};
+
+// return all diagonal strings of 2D array
+const diagonalString = letters => {
+  // define horizontal length as width
+  const width = letters[0].length;
+  // define vertical length as height
+  const height = letters.length;
+  // create an array variable to hold diagonal strings
+  const result = [];
+  // create a variable to hold starting indices of letters
+  const dIndex = [];
+
+  // helper function to add a diagonal line of string to result array
+  const diagonalHelper = (index, direction) => {
+    // if string direction is right or left, index either increments or decrements
+    const multiple = direction === "right" ? 1 : -1;
+    // create a variable to hold a line of string
+    const line = [];
+  
+    for (let j = 0; j < Math.max(width, height) - 1; j++) {
+      // array index will always increment in both right or left directions
+      const arrayIndex = index[0] + j;
+      // item index will increment or decrement depending on the direction of string
+      const itemIndex = index[1] + multiple * j;
+      // check if the index exists within the 2D array
+      if (Array.isArray(letters[arrayIndex]) && itemIndex >= 0 && itemIndex < width) {
+        // push the letter into line array
+        line.push(letters[arrayIndex][itemIndex]);
+      } else break;
+    }
+    // concatenate the array elements and add to result
+    result.push(line.join(''));
+  };
+
+  // iterate through top, left, right sides of 2D array
+  for (let i = 0; i < width + height * 2 - 2; i++) {
+    if (i < width) {
+      // push every item of first array as starting index
+      dIndex.push([0, i]);
+      // find both left-to-right and right-to-left diagonal string lines
+      diagonalHelper(dIndex[i], "right");
+      diagonalHelper(dIndex[i], "left");
+    } else if (i < width + height - 1) {
+      // after dIndex reaches [0, width], push the first items of arrays
+      dIndex.push([i - width + 1, 0]);
+      // only find left-to-right lines starting from dIndex
+      diagonalHelper(dIndex[i], "right");
+    } else {
+      // after dIndex reaches [height, 0], push the last items of arrays
+      dIndex.push([i - width - height + 2, width]);
+      // only find right-to-left lines
+      diagonalHelper(dIndex[i], "left");
+    }
+  }
+
+  return result;
+};
+
+// search for word from letters 2D array
+const wordSearch = (letters, word) => {
+  // create a flag to tell whether the word is found or not
+  let wordFound = false;
+  // if letters is not a 2D array or word is a falsy value, return false
+  if (!Array.isArray(letters[0]) || !word) return false;
+
+  // all horizontal string values of letters array, including reversed strings
+  const horizontalJoin = addBWard(letters.map(ls => ls.join('')));
+  // all vertical string values
+  const verticalJoin = addBWard(transpose(letters).map(ls => ls.join('')));
+  // all diagonal string values
+  const diagonalJoin = addBWard(diagonalString(letters));
+  // set all joint string values in one array
+  const joins = [horizontalJoin, verticalJoin, diagonalJoin];
+
+  // iterate through each array of string
+  joins.forEach(direction => {
+    // for each line of string,
+    for (const line of direction) {
+      // if the word is found, flag becomes true
+      if (line.includes(word)) wordFound = true;
+    }
+  });
+
+  // if the word is not found, function will return the original false flag
+  return wordFound;
+};
+
+
+module.exports = wordSearch;
